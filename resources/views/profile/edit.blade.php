77a6bf54033ea8324e66fc0f79d0c4458c006e78
jabirv <jabir.mohamdi@outlook.com>
2024-11-06 11:19:09 +0100

    Profile : Mise en place front

--- resources/views/profile/edit.blade.php
+++ resources/views/profile/edit.blade.php
@@ -5,6 +5,17 @@
         </h2>
     </x-slot>
 
+    <div class="py-12"> 
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg flex justify-center items-center">
+                <div class="max-w-xl w-full">
+                    @include('profile.partials.detail-and-stats')
+                </div>
+            </div>
+        </div>
+    </div>
+
+
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
