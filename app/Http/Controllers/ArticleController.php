85e851c5b1fcade5310ed1273f1062bf87e674ea
Maïssa Abdelli <maissaabdelli-13@outlook.fr>
2024-11-06 16:03:08 +0100

    implementer commentaire dans bbd

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Article;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Comment;
 
 class ArticleController extends Controller
 {
@@ -82,4 +83,29 @@ class ArticleController extends Controller
         $article = Article::findOrFail($id);
         return view('articles.show', compact('article'));
     }
+
+    public function storeComment(Request $request, $articleId)
+    {
+        // Validation des champs du formulaire
+        $request->validate([
+            'comment' => 'required|string|max:1000', // Valider la longueur du commentaire
+        ], [
+            'comment.required' => 'Le commentaire ne peut pas être vide.',
+            'comment.max' => 'Le commentaire ne peut pas dépasser 1000 caractères.',
+        ]);
+
+        // Récupérer l'article
+        $article = Article::findOrFail($articleId);
+
+        // Créer le commentaire
+        $comment = new Comment();
+        $comment->comment = $request->input('comment');
+        $comment->article= $article->id;
+        $comment->user = Auth::id(); // Assurez-vous que l'utilisateur est connecté
+        $comment->save();
+
+        // Rediriger l'utilisateur vers l'article avec un message de succès
+        return redirect()->route('articles.show', $article->id)->with('success', 'Commentaire ajouté avec succès');
+    }
+
 }
