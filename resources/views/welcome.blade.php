23605381d199224ee204fce474d43469d98f15ce
Axelle <axelle.flower@gmail.com>
2024-11-06 09:09:41 +0100

    Accueil guest

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
                                 </div>
                             </div>
                         </div>
-                        <section class="py-12 mt-12">
+                        <section class="py-12 mt-12 mb-12">
                             <div class="max-w-6xl mx-auto text-center">
                                 <h2 class="text-3xl font-semibold text-gray-900">Écosense, qu'est-ce que c'est ?</h2>
                                 <div class="mt-8 flex flex-col items-center gap-6 md:flex-row md:justify-center">
@@ -111,48 +111,48 @@
                                 </div>
                             </div>
                         </section>
-                        <div class="overflow-hidden py-24 sm:py-32 w-full">
+                        <div class="overflow-hidden w-full">
                             <div class="bg-[#243E25] bg-opacity-60 w-full">
                                 <div class="mx-auto px-6 lg:px-8">
                                     <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                                    <div class="lg:pr-8 lg:pt-4">
-                                        <div class="lg:max-w-lg">
-                                        <h2 class="text-base/7 font-semibold text-indigo-600">Deploy faster</h2>
-                                        <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">A better workflow</p>
-                                        <p class="mt-6 text-lg/8 text-gray-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</p>
-                                        <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
-                                            <div class="relative pl-9">
-                                            <dt class="inline font-semibold text-gray-900">
-                                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                                <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 0 1-1.44-8.765 4.5 4.5 0 0 1 8.302-3.046 3.5 3.5 0 0 1 4.504 4.272A4 4 0 0 1 15 17H5.5Zm3.75-2.75a.75.75 0 0 0 1.5 0V9.66l1.95 2.1a.75.75 0 1 0 1.1-1.02l-3.25-3.5a.75.75 0 0 0-1.1 0l-3.25 3.5a.75.75 0 1 0 1.1 1.02l1.95-2.1v4.59Z" clip-rule="evenodd" />
-                                                </svg>
-                                                Push to deploy.
-                                            </dt>
-                                            <dd class="inline">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</dd>
+                                        <div class="lg:pr-8 lg:pt-4">
+                                            <div class="lg:max-w-lg">
+                                                <h2 class="text-3xl sm:text-5xl font-bold text-pretty text-white">
+                                                    Rejoignez ÉcoSense pour découvrir un monde d'apprentissage et de défis écologiques!
+                                                </h2>
+                                                <p class="mt-6 text-lg/8 text-white">
+                                                    En vous connectant, vous aurez accès à :
+                                                </p>
+                                                <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
+                                                    <div class="relative pl-9">
+                                                        <dt class="inline font-semibold text-white">
+                                                            <span class="mr-2">•</span>Des articles exclusifs pour approfondir vos connaissances
+                                                        </dt>
+                                                    </div>
+                                                    <div class="relative pl-9">
+                                                        <dt class="inline font-semibold text-white">
+                                                            <span class="mr-2">•</span>Des formations interactives pour adopter des gestes durables
+                                                        </dt>
+                                                    </div>
+                                                    <div class="relative pl-9">
+                                                        <dt class="inline font-semibold text-white">
+                                                            <span class="mr-2">•</span>Un système de points XP pour récompenser vos progrès
+                                                        </dt>
+                                                    </div>
+                                                    <div class="relative pl-9">
+                                                        <dt class="inline font-semibold text-white">
+                                                            <span class="mr-2">•</span>Un classement pour vous comparer à vos amis et à la communauté
+                                                        </dt>
+                                                    </div>
+                                                    <div class="relative pl-9">
+                                                        <dt class="inline font-semibold text-white">
+                                                            <span class="mr-2">•</span>Des défis et missions pour gagner encore plus de points !
+                                                        </dt>
+                                                    </div>
+                                                </dl>
                                             </div>
-                                            <div class="relative pl-9">
-                                            <dt class="inline font-semibold text-gray-900">
-                                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                                <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z" clip-rule="evenodd" />
-                                                </svg>
-                                                SSL certificates.
-                                            </dt>
-                                            <dd class="inline">Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</dd>
-                                            </div>
-                                            <div class="relative pl-9">
-                                            <dt class="inline font-semibold text-gray-900">
-                                                <svg class="absolute left-1 top-1 h-5 w-5 text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true" data-slot="icon">
-                                                <path d="M4.632 3.533A2 2 0 0 1 6.577 2h6.846a2 2 0 0 1 1.945 1.533l1.976 8.234A3.489 3.489 0 0 0 16 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234Z" />
-                                                <path fill-rule="evenodd" d="M4 13a2 2 0 1 0 0 4h12a2 2 0 1 0 0-4H4Zm11.24 2a.75.75 0 0 1 .75-.75H16a.75.75 0 0 1 .75.75v.01a.75.75 0 0 1-.75.75h-.01a.75.75 0 0 1-.75-.75V15Zm-2.25-.75a.75.75 0 0 0-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75h-.01Z" clip-rule="evenodd" />
-                                                </svg>
-                                                Database backups.
-                                            </dt>
-                                            <dd class="inline">Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</dd>
-                                            </div>
-                                        </dl>
                                         </div>
-                                    </div>
-                                    <img src="https://image.noelshack.com/fichiers/2024/45/2/1730820813-mockups.jpg" alt="Product screenshot" class="max-w-none rounded-xl md:-ml-4 lg:-ml-0 w-full">
+                                        <img src="https://image.noelshack.com/fichiers/2024/45/3/1730878019-mockups.jpg" alt="Product screenshot" class="max-w-none rounded-xl md:-ml-4 lg:-ml-0 w-full">
                                     </div>
                                 </div>
                             </div>
