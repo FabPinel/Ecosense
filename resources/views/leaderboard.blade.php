908fae712aeba3722fff58a42670e2d43618d324
Maïssa Abdelli <maissaabdelli-13@outlook.fr>
2024-11-07 14:44:42 +0100

    explication:grade

--- resources/views/leaderboard.blade.php
+++ resources/views/leaderboard.blade.php
@@ -46,6 +46,17 @@
                             </li>
                         @endforeach
                     </ul>
+                    <!-- Explications des grades en fonction du score -->
+                    <div class="mt-8">
+                        <h2 class="text-lg font-semibold text-gray-900 mb-4">{{ __('Explications des grades :') }}</h2>
+                        <ul class="list-disc pl-6 space-y-2">
+                            <li><span class="font-semibold">0-99 points :</span> "Graine d'Espoir" 🌱 – Les nouveaux arrivants, qui commencent leur aventure écologique.</li>
+                            <li><span class="font-semibold">100-299 points :</span> "Ami des Abeilles" 🌿 – Les utilisateurs qui montrent un début d’engagement pour l'écologie.</li>
+                            <li><span class="font-semibold">300-599 points :</span> "Éclaireur des forêts" 🐦 – Des utilisateurs de plus en plus actifs qui protègent leur environnement.</li>
+                            <li><span class="font-semibold">600-999 points :</span> "Gardien de la Terre" 🌾 – Un utilisateur qui explore activement des moyens d’aider la planète.</li>
+                            <li><span class="font-semibold">+1000 points :</span> "Héros de la Terre" 🌍 – Un modèle d'inspiration pour la communauté écologique.</li>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
